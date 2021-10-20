15f1463d8b3998d12a840b455498851e306474fa
itsd3vil <91783383+itsd3vil@users.noreply.github.com>
2021-10-21 00:54:27 +0530

    Update alive.py

--- alive.py
+++ alive.py
@@ -5,27 +5,15 @@
 import time
 import requests
 import os
-from dotenv import load_dotenv
 
-load_dotenv('config.env')
-
+BASE_URL = os.environ.get('BASE_URL_OF_BOT', None)
 try:
-    BASE_URL = os.environ.get('BASE_URL_OF_BOT', None)
     if len(BASE_URL) == 0:
         BASE_URL = None
-except KeyError:
+except:
     BASE_URL = None
-
-try:
-    IS_VPS = os.environ.get('IS_VPS', 'False')
-    if IS_VPS.lower() == 'true':
-        IS_VPS = True
-    else:
-        IS_VPS = False
-except KeyError:
-    IS_VPS = False
-
-if not IS_VPS and BASE_URL is not None:
+PORT = os.environ.get('PORT', None)
+if PORT is not None and BASE_URL is not None:
     while True:
-        time.sleep(1000)
+        time.sleep(600)
         status = requests.get(BASE_URL).status_code
